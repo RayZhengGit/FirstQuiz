aecf1c0e3b6cf52f48d20c549fea94990f86134a
Raymond Zheng <raymondzheng@Laptop.localdomain>
2022-02-03 19:20:17 -0500

    first project

---
+++ Source/firstproject/firstprojectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "firstprojectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API AfirstprojectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
